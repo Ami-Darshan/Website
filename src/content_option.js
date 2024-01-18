92e2c9df86f7cd3f77f5ab15d05e2bb899641a57
Ami-Darshan <97103457+Ami-Darshan@users.noreply.github.com>
2024-01-18 10:43:47 +0545

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,146 @@
+const logotext = "Darshan";
+const meta = {
+    title: "Darshan Poudel",
+    description: "I’m Darshan Poudel data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Darshan Poudel",
+    animated: {
+        first: "I'm a programmer",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Sup, I am Darshan Poudel, a scholar enrolled in the eleventh grade at Motherland School in Nepal. I am fervently dedicated to my academic pursuits and enthusiastic about delving into novel avenues for both learning and personal development.",
+    your_img_url: "https://i.ibb.co/GcZXg1J/403629075-196130996883999-7154420793880210637-n.jpg",
+};
+
+const dataabout = {
+    title: "Abit About Me",
+    aboutme: "Now in my eleventh year of school, I am deeply interested in robotics and began my coding journey when I was eleven years old, in the ninth grade. I will shortly be revealing my skillfully constructed basic robot.",
+};
+const worktimeline = [{
+        jobtitle: "Programmer of week",
+        where: "Nepal, Pokhara",
+        date: "2023",
+    },
+    {
+        jobtitle: "Programmer of week",
+        where: "Nepal, Kathmandu",
+        date: "2022",
+    },
+    {
+        jobtitle: "Programmer of week",
+        where: "Nepal,Kathmandu",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I possess over 10 months of expertise in the realm of Ui/UX design, having crafted more than five user interfaces for presentations while working as a freelancer.",
+    },
+    {
+        title: "Full Stack Development",
+        description: "I am a full-stack developer with a track record of building numerous websites over the past 1.5 years.",
+    },
+    {
+        title: "Ethical Hacking",
+        description: "I am an ethical hacker and serve as the leader of a team comprising four individuals, all of whom are my former schoolmates.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://motherland.edu.np/wp-content/uploads/2019/01/play.jpg",
+        description: "Please click to preview the institution where I am poised to complete my 12th-grade education.",
+        link: "https://motherland.edu.np/",
+    },
+    {
+        img: "https://i.ibb.co/cTkZ0cB/miniland.png",
+        description: "Please click to explore the school where I successfully concluded my 10th-grade studies.",
+        link: "https://www.facebook.com/miniland.school/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://www.youtube.com/watch?v=6nSwVFIQosY&t=79s",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "pdldarshan008@gmail.com",
+    YOUR_FONE: "9702627315",
+    description: "Feel free to reach out to me using the provided information; however, please be aware that there might be a delay in my response",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_n487cwq",
+    YOUR_TEMPLATE_ID: "template_6c13rxp",
+    YOUR_USER_ID: "pn6wxwruhpaFfpJmL",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://www.facebook.com/profile.php?id=100084426002544",
+    linkedin: "https://www.linkedin.com/in/darshan-mob/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
